28ad39c5320358831bde8ffd8c608710e5c03c8f
Michał Mielczarek <Michal.Mielczarek@gmail.com>
2018-06-05 15:56:37 +0200

    pracuj w domu ostro

---
+++ src/test/java/pl/sda/tdd3/AlbumModifierTest.java
@@ -0,0 +1,47 @@
+package pl.sda.tdd3;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class AlbumModifierTest {
+
+
+    AlbumModifier albumModifier = new AlbumModifier();
+
+
+    @Test
+    public void testAlbumStorageCreator() throws IOException {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    public void testAddAlbum(AlbumModel album) throws IOException {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    @Test
+    public void testSubtractAlbum(AlbumModel album) throws IOException {
+        //given
+
+        //when
+
+        //then
+
+    }
+
+
+
+
+
+}
