5605d79119afe956bbc0b274bf5b6bec7b5eecdf
Michał Mielczarek <31986085+YoManZSiedlc@users.noreply.github.com>
2018-07-16 05:59:57 +0200

    MM. home

--- src/main/java/pl/sda/tdd3/AlbumReadWrite.java
+++ src/main/java/pl/sda/tdd3/AlbumReadWrite.java
@@ -1,10 +1,18 @@
 package pl.sda.tdd3;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class AlbumReadWrite {
+
     public Map<Integer, AlbumModel> readAlbumFromFile(String path) throws IOException {
-        return null;
+
+     return null;
+    }
+
+    public void writeAlbumToFile (String path, HashMap<Integer, AlbumModel> map)throws IOException{
+        return;
     }
 }
